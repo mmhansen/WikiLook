91a21ef0fce5f8d5caa6087eb22064b12ad0c84a
Michael Hansen <hansen.mmb@gmail.com>
2016-08-26 15:35:06 -0400

    Deisgn and coloring

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,76 @@
-var luckyText = [
-  {
-    Johnnie: "how to quit drinking"
-  },{
-    Homer: "achillies"
-  },{
-    Euclid: "platonic solids"
-  },{
-    Epictetus: "stoic"
-  },{
-    Descartes: "do I exist"
-  },{
-    Quixote: "windmills"
-  },{
-    God: "flying spaghetti monster"
-  },{
-    Bach: "matthew passion"
-  },{
-    Aristotle: "depression"
-  },{
-    Hungry: "how to make a sandwhich"
-  }
-];
-var random10;
-function getLucky() {
-  random10 = Math.floor(Math.random()*10);
-}
+
+///////////////////////////////////////////////
+// Switch between Landing and Content Styles //
+///////////////////////////////////////////////
 
 $(document).ready(function(){
-  
-  var feeling;
-  function getFeeling(){
-      for(property in luckyText[random10]){
-      feeling = property;
-    };
- }
-    
+  function contentCss(){
+    $('#header').removeClass('landing');
+    $('#header').removeClass('col-md-offset-3');
+    $('#header').addClass('content');
+  };
+
+
+  $("#search").keypress(function(){
+    contentCss();
+  })
+   // reset to main page
+  $('h1').click(function(){
+    $('#header').removeClass('content');
+    $('#header').addClass('landing');
+    $('#header').addClass('col-md-offset-3');    
+  })
+});
+
+////////////////////////////////////////////
+// Animation for I'm feeling Lucky button //
+////////////////////////////////////////////
+
+$(document).ready(function(){
+  // initial Display
   $('#random').text("I'm feeling Johnnie");
   
+  var imFeeling = ["Johnnie", "Homer", "Euclid", "Epictetus", "Descartes", "Quixote", "God", "Bach", "Aristotle", "Hungry"];
+  var randomNum;
+  var selectedFeeling;
+
+  function getRandom() {
+    randomNum = Math.floor(Math.random()*10);
+    selectedFeeling = imFeeling[randomNum];
+  }
   
   $('#random').mouseenter(function(){
-    // returns a random number 10
-    getLucky();
-    getFeeling();
-    $('#random').text("I'm feeling "+feeling); 
- 
-    
+    getRandom();
+    $('#random').text("I'm feeling "+selectedFeeling); 
   });
+  
   $('#random').mouseleave(function(){
     $(this).text("I'm feeling Johnnie");
   });
-  
-  
-  $("#search").keypress(function(){
-    $('.btn').css("display","none");    
-    $('html').css({"margin":"0px",
-                   "padding":"0px"})
-    $('body').css({"margin":"0px",
-                   "padding":"0px"})
-    // create header bar
-    $('.header').css({"background":"#E3F2FD",
-                      "margin":"0",
-                      "padding":"0",
-                      "text-align":"left",
-                      "box-shadow":"6px 6px 6px rgba(0,0,0,0.4)"});
-    // alter logo text
-    $('h1').css({"margin":"0",
-                 "padding":"15",
-                "display":"inline-block",
-                "font-size":"2em"})
-    
-    //alter search bar
-    $('input').css({"display":"inline-block",
-                   "margin": "0",
-                   "vertical-align":"top",
-                   "margin-top":"14px"})
-    
-    //end keypress function
-  })
-   // reset to main page
- 
-  $('h1').click(function(){
-    $(".header").removeAttr('style');
-    $("h1").removeAttr('style');
-    $("input").removeAttr('style');
-    $("body").removeAttr('style');
-    $("html").removeAttr('style');
-    $(".btn").css("display","inline-block");
-  })
 
+})
+
+////////////////////////////////////////////////
+// Autocomplete List dropdown from search bar //
+////////////////////////////////////////////////
+
+
+
+////////////////////////////
+// Display Search Results //
+////////////////////////////
+
+
+////////////////////////////////////
+// Display full page when clicked //
+////////////////////////////////////
+
+
+
+
+
+
+/*
   // display function
   
   var tags = [];
@@ -123,5 +106,8 @@ $(document).ready(function(){
   
   
   
-// end document ready
-});
+
+
+
+
+*/
