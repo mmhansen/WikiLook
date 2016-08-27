ae325239b208ec9cdf3bbae8e6cb46a12760cc50
Michael Hansen <hansen.mmb@gmail.com>
2016-08-27 08:50:28 -0400

    Finished content display, started autocomplete

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,18 @@ $(document).ready(function(){
   };
 
 
-  $("#search").keypress(function(){
+  $(".search").keypress(function(){
+
     contentCss();
   })
    // reset to main page
   $('h1').click(function(){
+    $('.results').empty()
     $('#header').removeClass('content');
+    $('.search').blur();
+
+    $('.search').val("")
+
     $('#header').addClass('landing');
     $('#header').addClass('col-md-offset-3');    
   })
@@ -53,12 +59,111 @@ $(document).ready(function(){
 ////////////////////////////////////////////////
 // Autocomplete List dropdown from search bar //
 ////////////////////////////////////////////////
+/*
+$(document).ready(function(){
 
 
+  var tags = []
+
+$('.search').on('input', function(){
+   
+    var URL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $('.search').val() + '&limit=5&namespace=0&format=json'
+          
+    function getTags(data) {
+      tags = data[1];
+      // console.log(tags);
+      $('#autocomplete').autocomplete({ source: tags } );
+     }
+    
+  $.get(URL, getTags, "jsonp")
+  });
+
+
+
+
+})
+*/
 
 ////////////////////////////
 // Display Search Results //
 ////////////////////////////
+var searchItem;
+
+// populate display field with search results
+function showResults (callback) {
+
+  for (var i = 0; i <= 9; i++) {
+    $(".results").append("<div class='result-list result-" + i + "'>" + "<span class='result-title title-" + i + "'></span>" + "<br>" +"<span class='result-snippet snippet-" + i + "'></span>" + "<br>" + "<span class='result-metadata metadata-" + i + "'></span>" + "</div>" );
+  }
+
+  for (var m = 0; m <= 9; m++) {
+    var title = callback.query.search[m].title;
+    var url = title.replace(/ /g, "_");
+    var timestamp = callback.query.search[m].timestamp;
+    timestamp = new Date(timestamp);
+
+    $(".title-" + m).html("<a href='https://en.wikipedia.org/wiki/" + url + "' target='_blank'>" + callback.query.search[m].title + "</a>");
+    $(".snippet-" + m).html(callback.query.search[m].snippet);
+    $(".metadata-" + m).html((callback.query.search[m].size/1000).toFixed(0) + "kb (" + callback.query.search[m].wordcount + " words) - " + timestamp);
+  }
+}
+
+function showError(keyword) {
+  $(".results").append( "<div class='error'> <p>Your search <span class='keyword'>" + keyword + "</span> did not match any documents.</p> <p>Suggestions:</p><li>Make sure that all words are spelled correctly.</li><li>Try different keywords.</li><li>Try more general keywords.</li></div> ");
+}
+
+// get info from wikipedia
+function ajax (keyword) { 
+  $.ajax({ 
+    url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
+    dataType: "jsonp",
+    success: function(response) {
+
+      console.log(response.query);
+
+      if (response.query.searchinfo.totalhits === 0) {
+        $('.results').empty()
+       showError(keyword) 
+      }
+
+      else {
+        $('.results').empty()
+        showResults(response)
+      }
+    }
+
+    ,
+    error: function () {
+      $('.results').empty()
+      $(".results").append( "<div class='error'> <p>Your search went awry! Please try again</p></div>" );
+    }
+
+  });
+}
+
+$(document).ready(function(){
+// active display on enter press
+  $('.search').keypress(function (e) {
+      if (e.which == '13') {
+
+        var searchItem = $('.search').val();
+
+        if (searchItem == ""){
+          $(".search").blur();
+          $('.results').empty()
+          $(".results").append( "<div class='error'> <p>Type something before you search!</p></div>" );
+
+        } else {
+
+          $(".search").blur();
+          ajax(searchItem);
+
+        }
+      }
+  });
+
+})
+
 
 
 ////////////////////////////////////
@@ -66,48 +171,3 @@ $(document).ready(function(){
 ////////////////////////////////////
 
 
-
-
-
-
-/*
-  // display function
-  
-  var tags = [];
-  $('#search').on('input', function(){
-   
-    var URL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $('#search').val() + '&limit=10&namespace=0&format=json'
-          
-    function getTags(data) {
-      tags = data[1];
-     // console.log(tags);
-    }
-    
-  $.get(URL, getTags, "jsonp")
-  
-  });
-  
-  
-  
-    function displayEach(){
-      
-      var item = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=' 
-       + 'He' + '&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max'
-      
-      $.get(item, function(data){
-                console.log(data);
-                }, jsonp)
-      // end display each
-    };
-  
-  
-  
-  displayEach();
-  
-  
-  
-
-
-
-
-*/
